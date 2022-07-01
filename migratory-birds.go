b820c74ad75455e99b52b642b6419c566083b360
MTalhaKaraca <89966125+MTalhaKaraca@users.noreply.github.com>
2022-07-01 21:12:12 +0300

    .

---
+++ migratory-birds.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true*/
+
+func main() {
+	arr := []int32{1, 4, 4, 4, 5, 3, 3, 3}
+	xd := migratoryBirds(arr)
+	fmt.Println(xd)
+}
+
+func migratoryBirds(arr []int32) int32 {
+	birds := make(map[int32]int32, len(arr))
+	for i := 0; i < len(arr); i++ {
+		birds[arr[i]]++
+	}
+
+	var biggest = birds[0]
+	for _, k := range birds {
+		if k > biggest {
+			biggest = k
+		}
+	}
+
+	i := 0
+	var results []int32
+	for k, j := range birds {
+		if j == biggest {
+			results = append(results, k)
+			i++
+		}
+	}
+	fmt.Println(results)
+	var final = results[0]
+	for m := 0; m < len(results); m++ {
+		if results[m] < final {
+			final = results[m]
+		}
+	}
+	return final
+}
